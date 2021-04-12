13365faaaa79c6df9ad318ab31260ddaad5ea943
Amelia Zecker <amelia.zecker@jackpinetech.com>
2021-04-11 23:47:43 -0400

    set app.js back to baseline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import LogoHero from "./components/LogoHero/LogoHero";
-import YearlyInfo from "./components/YearlyInfo/YearlyInfo";
 
 function App() {
   return (
     <div className="App">
       <LogoHero />
-      <YearlyInfo />
     </div>
   );
 }
